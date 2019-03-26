86b857b4af9b31c30c1f78718e0a1b55c023c845
Max <maximomacchi@gmail.com>
2019-03-25 22:24:24 -0700

    Wrote failing unit test

--- project2-skamn/tests/test_environment.py
+++ project2-skamn/tests/test_environment.py
@@ -4,3 +4,7 @@ from test_functions import dummy_functions
 
 def test_environment():
     assert dummy_functions.dummy_function() == 4
+
+
+def test_supposed_to_fail():
+    assert dummy_functions.dummy_function() == 2
