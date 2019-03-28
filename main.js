4f2c7713098fa1dfed89a790e76a669165c6c82a
Gurkirt Heerey <g_heerey@u.pacific.edu>
2019-03-28 15:55:54 -0700

    electron-reload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // Modules to control application life and create native browser window
+require('electron-reload')(__dirname)
 const {app, BrowserWindow} = require('electron')
+
+console.log("HELO WORLD");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
