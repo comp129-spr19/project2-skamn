333ed49b615e07d944ad18666bb15026862c618d
Max <maximomacchi@gmail.com>
2019-03-28 01:07:47 -0700

    User error prevented so that only integers can be used as inputs

--- daily_goal.js
+++ daily_goal.js
@@ -1,6 +1,14 @@
 let dailyGoal = 0;
 
-function setGoal() {
+function setGoal(e) {
+  e.preventDefault();
   let dailyGoalUserInput = document.getElementById("dailyGoalUserInput");
   dailyGoal = dailyGoalUserInput.value;
+  if (dailyGoal == "") dailyGoal = 0;
+  else if (isNaN(dailyGoal)) {
+    alert("Please enter a number");
+    return;
+  }
+  let dailyGoalDisplay = document.getElementById("currentDailyGoal");
+  dailyGoalDisplay.innerHTML = dailyGoal;
 }
