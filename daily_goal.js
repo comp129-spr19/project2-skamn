5399c42db05a4e2b89e6246962ecceae05d2dcde
Max <maximomacchi@gmail.com>
2019-03-28 16:01:36 -0700

    Daily water goal is now forced to be positive number

--- daily_goal.js
+++ daily_goal.js
@@ -4,9 +4,12 @@ function setGoal(e) {
   e.preventDefault();
   let dailyGoalUserInput = document.getElementById("dailyGoalUserInput");
   dailyGoal = dailyGoalUserInput.value;
-  if (dailyGoal == "") dailyGoal = 0;
+  if (dailyGoal == "") dailyGoal = 1;
   else if (isNaN(dailyGoal)) {
     alert("Please enter a number");
+    return;
+  } else if (dailyGoal < 1) {
+    alert("Plese enter a positive number");
     return;
   }
   let dailyGoalDisplay = document.getElementById("currentDailyGoal");
