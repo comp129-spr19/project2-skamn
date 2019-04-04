4419fc3b99a56aa70b1381eb2ff1c70499abce1a
Max <maximomacchi@gmail.com>
2019-04-03 21:44:13 -0700

    Wrote unit test for checking if  dailyGoalMet becomes true when user surpasses goal

--- tests/daily_goal.test.js
+++ tests/daily_goal.test.js
@@ -1,4 +1,9 @@
-const { getDailyGoal, setGoal } = require("../daily_goal");
+const {
+  getDailyGoal,
+  setGoal,
+  setWaterDrankRecently,
+  getDailyGoalMet
+} = require("../daily_goal");
 
 test("initial value of dailyGoal should be 0", () => {
   expect(getDailyGoal()).toBe(0);
@@ -26,4 +31,22 @@ test("setGoal should change dailyGoal to 5", () => {
   const currDailyGoalVal = document.getElementById("currentDailyGoal")
     .innerHTML;
   expect(currDailyGoalVal).toBe("5");
+});
+
+test("dailyGoalMet should equal true when user surpasses dailyGoal", () => {
+  // Setup document body for testing
+  document.body.innerHTML =
+    '<div id="totalWaterDrankToday">0</div>' +
+    '<form onsubmit="setWaterDrankRecently(event)">' +
+    '<input type="number" id="waterDrankRecently" />' +
+    "</form>" +
+    '<div id="currentDailyGoal">3</div>' +
+    '<script src="../daily_goal.js"></script>';
+
+  // Simulate user reaching daily goal
+  document.getElementById("waterDrankRecently").value = "5";
+  setWaterDrankRecently();
+
+  // Check if daily goal has been met
+  expect(getDailyGoalMet()).toBe(true);
 });
