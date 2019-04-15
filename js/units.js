01aa17ddd873b61a2eb2b8c63ebb5d22aaa580c9
Ali <a_noorani@u.pacific.edu>
2019-04-14 20:10:11 -0700

    implemented logic to convert units

--- js/units.js
+++ js/units.js
@@ -1,16 +1,24 @@
 const convert = require("convert-units");
 
-function convertHTMLUnits(fromUnits, toUnits) {
+function convertHTMLUnits(toUnits) {
+  const fromUnits = getUnits();
   const convertedDailyGoalVal = convert(getDailyGoal())
     .from(fromUnits)
     .to(toUnits);
-  const convertedTotalWaterDrankToday = convert(
-    getTotalWaterDrankToday()
-      .from(fromUnits)
-      .to(toUnits)
-  );
+  const convertedTotalWaterDrankToday = convert(getTotalWaterDrankToday())
+    .from(fromUnits)
+    .to(toUnits);
 
   setDailyGoal(convertedDailyGoalVal);
   setTotalWaterDrankToday(convertedTotalWaterDrankToday);
+  setUnits(toUnits);
   updateDependentComponents();
+}
+
+function getUnits() {
+  return document.getElementById("currentUnits").innerHTML;
+}
+
+function setUnits(units) {
+  document.getElementById("currentUnits").innerHTML = units;
 }
