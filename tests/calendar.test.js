e649ae5acaec2fb268e0df667f0cddb926a6a01b
Nathan Chica <n_chica@u.pacific.edu>
2019-04-16 23:49:21 -0700

    added a test for daysInMonth fn

--- tests/calendar.test.js
+++ tests/calendar.test.js
@@ -11,3 +11,7 @@ test("April 2019 should have 30 days", () => {
 test("February 2019 should have 28 days", () => {
   expect(daysInMonth(1, 2019)).toBe(28);
 });
+
+test("March 2018 should have 31 days", () => {
+  expect(daysInMonth(2, 2018)).toBe(31);
+});
