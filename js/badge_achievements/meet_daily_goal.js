b7687eced872f0ccf204a00d328838c0c2533228
Sahib Gosal <s_gosal2@u.pacific.edu>
2019-04-21 00:16:58 -0700

    Adding logic for meeting daily goal for 7 days

--- js/badge_achievements/meet_daily_goal.js
+++ js/badge_achievements/meet_daily_goal.js
@@ -5,3 +5,11 @@ function meetDailyGoalOnceBadge() {
         saveBadgeAchievements(badgeName, date.toLocaleDateString());
     }
 }
+
+function meetDailyGoalSevenBadge() {
+    let badgeName = "meetDailyGoalSeven";
+    if (!checkIfBadgeAchieved(badgeName)) {
+        let date = new Date();
+        saveBadgeAchievements(badgeName, date.toLocaleDateString());
+    }
+}
