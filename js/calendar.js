fd96572a96ee2bfc8db1ea81384e1b7b1cb1a6c5
Ali <a_noorani@u.pacific.edu>
2019-04-21 19:40:11 -0700

    started creating dynamic table for calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,4 +1,75 @@
-// const firstDay = new Date(year, month).getDay();
+let todayDateFull;
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+window.onload = initPage;
+
+function initPage() {
+  todayDateFull = new Date();
+  let todayDate = todayDateFull.getDate();
+  let currentMonthIndex = todayDateFull.getMonth();
+  let currentMonth = months[currentMonthIndex];
+  let currentYear = todayDateFull.getFullYear();
+  console.log(months[currentMonthIndex]);
+  console.log(todayDate);
+  console.log(currentYear);
+  let todayDateSimple =
+    months[currentMonthIndex] + " " + todayDate + ", " + currentYear;
+
+  document.getElementById("todayDate").innerHTML = todayDateSimple;
+  showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+  console.log("HELLO");
+  let i = 0;
+  let j = 0;
+  let button;
+  let cellText;
+  let week = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let body = document.getElementsByTagName("body")[0];
+  let tbl = document.createElement("table");
+  let tblBody = document.createElement("tbody");
+  let breakTag = document.createElement("BR");
+  for (i = 0; i < 6; i++) {
+    let row = document.createElement("tr");
+    // button = document.createElement("button");
+    for (j = 0; j < 7; j++) {
+      let cell = document.createElement("td");
+      if (i == 0) {
+        cellText = document.createTextNode("HELLO");
+      }
+      //   cellText = document.createTextNode("cell in row " + i + ", column " + j);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+      //   row.append(button);
+    }
+    tblBody.appendChild(row);
+  }
+  tbl.appendChild(tblBody);
+  body.appendChild(tbl);
+  tbl.setAttribute("border", "4");
+}
 
 function daysInMonth(iMonth, iYear) {
   return 32 - new Date(iYear, iMonth, 32).getDate();
