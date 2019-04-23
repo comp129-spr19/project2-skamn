aa704d48a9db6efcc0637725a9728656b347c8c5
Nathan Chica <n_chica@u.pacific.edu>
2019-04-23 10:37:06 -0700

    month strings

--- js/calendar.js
+++ js/calendar.js
@@ -4,6 +4,41 @@ function daysInMonth(iMonth, iYear) {
   return 32 - new Date(iYear, iMonth, 32).getDate();
 }
 
+function initCalendar() {
+  document.getElementById("month-display").innerHTML = (new Date).
+}
+
+function getMonthString(monthVal) {
+  switch(monthVal) {
+    case 0:
+      return "January";
+      case 0:
+      return "February";
+      case 0:
+      return "March";
+      case 0:
+      return "April";
+      case 0:
+      return "June";
+      case 0:
+      return "July";
+      case 0:
+      return "August";
+      case 0:
+      return "September";
+      case 0:
+      return "October";
+      case 0:
+      return "November";
+      case 0:
+      return "December";
+      case 0:
+      return "January";
+    default:
+      return "";
+  }
+}
+
 module.exports = {
   daysInMonth
 };
