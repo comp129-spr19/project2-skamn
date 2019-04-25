68db060f9d376031209c4e11b78f5482c3256d6f
Gurkirt Heerey <g_heerey@u.pacific.edu>
2019-04-25 12:21:13 -0700

    Clicking on calendar avaliable now

--- js/calendar.js
+++ js/calendar.js
@@ -2,20 +2,7 @@ let today = new Date();
 let selectYear = document.getElementById("year");
 let selectMonth = document.getElementById("month");
 
-let months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec"
-];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 let monthAndYear = document.getElementById("monthAndYear");
 
@@ -40,8 +27,8 @@ function jump() {
 function showCalendar(month, year) {
   let firstDay = new Date(year, month).getDay();
   let daysInMonth = getDaysInMonth(month, year);
-
   let tbl = document.getElementById("calendar-body"); // body of the calendar
+
 
   // clearing all previous cells
   tbl.innerHTML = "";
@@ -59,6 +46,7 @@ function showCalendar(month, year) {
 
     //creating individual cells, filing them up with data.
     for (let j = 0; j < 7; j++) {
+      
       if (i === 0 && j < firstDay) {
         let cell = document.createElement("td");
         let cellText = document.createTextNode("");
@@ -75,19 +63,27 @@ function showCalendar(month, year) {
           month === today.getMonth()
         ) {
           cell.classList.add("bg-info");
+          cell.classList.add("../badge-images/goal-30days.png");
         } // color today's date
         cell.appendChild(cellText);
         row.appendChild(cell);
         date++;
       }
     }
-
     tbl.appendChild(row); // appending each row into calendar body.
   }
+
 }
 
 function getDaysInMonth(month, year) {
   return 32 - new Date(year, month, 32).getDate();
+}
+
+function viewBadgesOnDate() {
+  let modal = document.getElementById("modals");
+  let span = document.getElementsByClassName("close")[0];
+  modal.style.display = "block";
+  location.reload();
 }
 
 module.exports = {
