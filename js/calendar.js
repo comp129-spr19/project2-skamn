1ad2a05d5a9d35a450f67a2c6c9ab5bd31e8ae75
Ali <a_noorani@u.pacific.edu>
2019-04-26 01:26:25 -0700

    modal appears after clicking on button inside date

--- js/calendar.js
+++ js/calendar.js
@@ -112,24 +112,33 @@ function makeModal(btn, valueText) {
     }
   }, "badgeachievements");
 
-  //   // Get the <span> element that closes the modal
-  //   let span = document.getElementsByClassName("close")[0];
+  //   function toggleModal() {
+  //     modal.toggle("show-modal");
+  //   }
 
-  //   btn.onclick = function() {
-  //     modal.style.display = "block";
-  //   };
-
-  //   // When the user clicks on <span> (x), close the modal
-  //   span.onclick = function() {
-  //     modal.style.display = "none";
-  //   };
-
-  //   // When the user clicks anywhere outside of the modal, close it
-  //   window.onclick = function(event) {
-  //     if (event.target == modal) {
-  //       modal.style.display = "none";
+  //   function windowOnClick(event) {
+  //     if (event.target === modal) {
+  //       toggleModal();
   //     }
-  //   };
+  //   }
+
+  // Displays Modal after clicking on box inside Calendar
+  modal.style.display = "block";
+
+  // Get the <span> element that closes the modal
+  let span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 }
 
 function getDaysInMonth(month, year) {
