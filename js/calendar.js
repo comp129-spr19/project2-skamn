df09473f5e87edabfddea79cfefd0731a6218fda
Ali <a_noorani@u.pacific.edu>
2019-04-26 01:10:21 -0700

    working on showing modal

--- js/calendar.js
+++ js/calendar.js
@@ -112,24 +112,24 @@ function makeModal(btn, valueText) {
     }
   }, "badgeachievements");
 
-  // Get the <span> element that closes the modal
-  let span = document.getElementsByClassName("close")[0];
+  //   // Get the <span> element that closes the modal
+  //   let span = document.getElementsByClassName("close")[0];
 
-  btn.onclick = function() {
-    modal.style.display = "block";
-  };
+  //   btn.onclick = function() {
+  //     modal.style.display = "block";
+  //   };
 
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  };
+  //   // When the user clicks on <span> (x), close the modal
+  //   span.onclick = function() {
+  //     modal.style.display = "none";
+  //   };
 
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
+  //   // When the user clicks anywhere outside of the modal, close it
+  //   window.onclick = function(event) {
+  //     if (event.target == modal) {
+  //       modal.style.display = "none";
+  //     }
+  //   };
 }
 
 function getDaysInMonth(month, year) {
